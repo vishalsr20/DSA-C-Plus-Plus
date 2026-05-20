a4c030477d1ec3966e67043cf32e5c0ac86061f0
Vishal Rathod <vishalrathod191918@gmail.com>
2026-05-20 12:33:29 +0530

    feat: Hashmap

---
+++ C++/Hashmap/basic.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+int main(){
+
+    //creation 
+    unordered_map<string , int>m;
+
+    // insertion
+    
+    //Type:1
+    pair<string,int>p = make_pair("Scorpio",9);
+    m.insert(p);
+
+    //Type:2
+    pair<string,int>p2("alto",2);
+    m.insert(p2);
+
+    //Type:3
+    m["fortuner"] = 10;
+
+
+    // Access
+    cout<<m.at("alto")<<endl;
+    cout<<m.at("Scorpio")<<endl;
+    cout<<m["fortuner"]<<endl;
+
+    //Search 
+    cout<<m.count("Scorpio")<<endl; // 1 => present and 0 => present 44
+
+    if(m.find("fortuner") != m.end()){
+        cout<<"Fortuner found"<<endl;
+    }else{
+        cout<<"Fortuner not found"<<endl;
+    }
+
+    cout<<m.size()<<endl;
+
+    cout<<"Printing the map values"<<endl;
+    for(auto i:m){
+        cout<<i.first<<"->"<<i.second<<endl;
+    }
+    return  0;
+}
